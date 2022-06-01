2b0ffe51f09f9a8798735aeb7fc8da8be696c7e2
nachojo <kjwjj486@gmail.com>
2022-06-01 13:51:17 +0900

    data insert

---
+++ region1.js
@@ -0,0 +1,23 @@
+let img = document.querySelector('img');
+
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#btn4");
+let btn5 = document.querySelector("#btn5");
+
+btn1.addEventListener('click', () => {
+    img.src = "평균거리1.png"
+})
+btn2.addEventListener('click', () => {
+    img.src = "평균거리2.png"
+})
+btn3.addEventListener('click', () => {
+    img.src = "평균거리3.png"
+})
+btn4.addEventListener('click', () => {
+    img.src = "평균거리4.png"
+})
+btn5.addEventListener('click', () => {
+    img.src = "평균거리5.png"
+})
